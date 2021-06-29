ed1797bf05a399dafc3c52d28d594dcdc7fbe4d6
peter.chen1024 <u0052017@gmail.com>
2021-06-29 18:17:42 +0800

    add 取得管理訂單列表

--- src/views/Dashboard/Orders.vue
+++ src/views/Dashboard/Orders.vue
@@ -1,5 +1,102 @@
 <template>
   <div class="container">
     <h1>訂單管理列表</h1>
+    <Loading :active="isLoading" :z-index="1060"></Loading>
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th>購買時間</th>
+          <th>Email</th>
+          <th>購買款項</th>
+          <th>應付金額</th>
+          <th>是否付款</th>
+          <th>編輯</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="(item, key) in orders" :key="key">
+          <tr v-if="orders.length" :class="{ 'text-secondary': !item.is_paid }">
+            <td>{{ $filters.date(item.create_at) }}</td>
+            <td><span v-text="item.user.email" v-if="item.user"></span></td>
+            <td>
+              <ul class="list-unstyled">
+                <li v-for="(product, i) in item.products" :key="i">
+                  {{ product.product.title }} 數量：{{ product.qty }}
+                  {{ product.product.unit }}
+                </li>
+              </ul>
+            </td>
+            <td class="text-right">{{ item.total }}</td>
+            <td>
+              <div class="form-check form-switch">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  :id="`paidSwitch${item.id}`"
+                  v-model="item.is_paid"
+                  @change="updatePaid(item)"
+                />
+                <label class="form-check-label" :for="`paidSwitch${item.id}`">
+                  <span v-if="item.is_paid">已付款</span>
+                  <span v-else>未付款</span>
+                </label>
+              </div>
+            </td>
+            <td>
+              <div class="btn-group">
+                <button
+                  class="btn btn-outline-primary btn-sm"
+                  type="button"
+                  @click="openModal(item)"
+                >
+                  檢視
+                </button>
+                <button
+                  class="btn btn-outline-danger btn-sm"
+                  type="button"
+                  @click="openDelOrderModal(item)"
+                >
+                  刪除
+                </button>
+              </div>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+    <div class="d-flex justify-content-center">
+      <Pagination :pages="pagination" @get-products="getOrders"></Pagination>
+    </div>
   </div>
 </template>
+<script>
+import Pagination from '@/components/Pagination.vue';
+
+export default {
+  data() {
+    return {
+      orders: {},
+      tempOrder: {},
+      pagination: {},
+      currentPage: 1,
+      isLoading: false,
+    };
+  },
+  components: { Pagination },
+  methods: {
+    getOrders(page = 1) {
+      this.currentPage = page;
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/orders?page=${page}`;
+      this.isLoading = true;
+      this.$http.get(url).then((res) => {
+        this.orders = res.data.orders;
+        this.pagination = res.data.pagination;
+        this.isLoading = false;
+      });
+    },
+  },
+  created() {
+    this.getOrders();
+  },
+};
+</script>
